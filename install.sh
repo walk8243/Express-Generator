206589e2386524fbfc349382f732923f41b6cae4
walk8243 <walk8243@gmail.com>
2018-07-24 00:28:13 +0900

    npm init

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+expect -c "
+spawn npm init
+expect \"package name:\"
+send \"\n\"
+expect \"version:\"
+send \"\n\"
+expect \"description:\"
+send \"\n\"
+expect \"entry point:\"
+send \"\n\"
+expect \"test command:\"
+send \"\n\"
+expect \"git repository:\"
+send \"\n\"
+expect \"keywords:\"
+send \"\n\"
+expect \"author:\"
+send \"\n\"
+expect \"license:\"
+send \"\n\"
+expect \"Is this OK?\"
+send \"\n\"
+expect \"$\"
+exit 0
+"
