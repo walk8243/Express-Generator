1bb1f2664be65eba2f6e1cce850e341422e84731
walk8243 <walk8243@gmail.com>
2018-07-29 19:39:11 +0900

    ファイルが無い時に監視メソッドが動かないように設定

--- watch.js
+++ watch.js
@@ -6,9 +6,13 @@ module.exports = (async () => {
   // sassを監視して、コンパイル
   var sassFiles = await sass.getSassFiles(),
       renderingSassFiles = await sass.getRenderingSassFiles();
-  watch.watchFile(sassFiles, (() => {sass.render(renderingSassFiles)}));
+  if(sassFiles.length) {
+    watch.watchFile(sassFiles, (() => {sass.render(renderingSassFiles)}));
+  }
 
   // jsを監視して、圧縮
   var jsFiles = await js.getJsFiles();
-  watch.watchFile(jsFiles, (() => {js.compress(jsFiles)}));
+  if(jsFiles.length) {
+    watch.watchFile(jsFiles, (() => {js.compress(jsFiles)}));
+  }
 })();
