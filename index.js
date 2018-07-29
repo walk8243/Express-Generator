16a6f3b7d380efce3a1af185ce08390cf6b7b4a5
walk8243 <walk8243@gmail.com>
2018-07-29 17:31:11 +0900

    jsの圧縮関数の実装

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 // sassを監視して、コンパイル
+// (async () => {
+//   const sass = require('./sass'),
+//         watch = require('./watch');
+//
+//   var sassFiles = await sass.getSassFiles(),
+//       renderingSassFiles = await sass.getRenderingSassFiles();
+//
+//   watch.watchFile(sassFiles, (() => {sass.render(renderingSassFiles)}));
+// })();
+
+// jsを監視して、圧縮
 (async () => {
-  const sass = require('./sass'),
+  const js = require('./js'),
         watch = require('./watch');
 
-  var sassFiles = await sass.getSassFiles(),
-      renderingSassFiles = await sass.getRenderingSassFiles();
+  var jsFiles = await js.getJsFiles();
 
-  watch.watchFile(sassFiles, (() => {sass.render(renderingSassFiles)}));
+  watch.watchFile(jsFiles, (() => {js.compress(jsFiles)}));
 })();
